b59464c0c7c3e3bfcc76b7500292a6832438ce51
atul <atulg0736@gmail.com>
2024-09-02 05:19:41 +0530

    static

--- zainly/settings.py
+++ zainly/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'dashboard.CustomUser'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,20 +124,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-# COMPRESS_ROOT = BASE_DIR / 'static'
-
-# COMPRESS_ENABLED = True
-
-# STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL="/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-# STATICFILES_DIRS =[
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, "static"),
+]
 
 # LOGIN_REDIRECT_URL = "/login"
 LOGOUT_REDIRECT_URL = "/login"
--- zainly/urls.py
+++ zainly/urls.py
@@ -11,9 +11,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('', include('dashboard.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-# handler404 = error_404_view
-# handler500 = error_500_view
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
