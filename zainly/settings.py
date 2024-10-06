9db2aae04690542d72dd568baf8e4b7b5c452c90
atul <atulg0736@gmail.com>
2024-10-06 17:25:24 +0530

    h

--- zainly/settings.py
+++ zainly/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7gnr974ckmmmh0l(l&ru^x3@ig)nxxbz85#1%kvrkxq=2baa19'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
